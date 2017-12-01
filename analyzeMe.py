84e301c5e36566d6aa1517cc706d4752e9227027
Daniel Santoro <daniel.d.santoro@gmail.com>
2017-11-30 23:36:11 -0500

    updates for new analysis

--- analyzeMe.py
+++ analyzeMe.py
@@ -1,6 +1,8 @@
 # imports
 from espnff import League
 from enum import Enum
+import matplotlib.pyplot as plt
+import statistics as stats
 
 # constants 
 WINS = 0
@@ -9,26 +11,31 @@ TIES = 2
 
 # league info
 league_id = 650880
-year = 2017
+year = 2016
 league = League(league_id, year)
 
 # TODO -> either (1) find which weeks have been completed, or (2) make this a command line argument
-numWeeks = 11
+numWeeks = 13
 
 
 class Team:
 	scores = []
 	record = []
 	rank = []
+	winDifferential = []
+	averageWinDiff = 0
+	stdDevWinDiff = 0
 
 	def __init__(self, name, scores) :
 		self.name = name
 		self.scores = scores
 		self.record = []
 		self.rank = []
+		self.winDifferential = []
 		for i in range(0, len(self.scores)) :
 			self.record.append([0,0,0])
 			self.rank.append(0)
+			self.winDifferential.append(0)
 
 	def addScore(self, score, week) :
 		self.scores.append(score)
@@ -73,6 +80,32 @@ def setRecords(teams) :
 							team.record[j][TIES] += 1;
 	return teams
 
+def setWinDifferential(teams) :
+	for week in range(0, numWeeks) :
+		for team in teams :
+			if week == 0 :
+				team.winDifferential[week] = team.record[week][WINS]
+			else :
+				team.winDifferential[week] = team.record[week][WINS] - team.record[week-1][WINS]
+	return teams
+
+def setStats(teams) :
+	for team in teams :
+		team.averageWinDiff = stats.mean(team.winDifferential[0:numWeeks])
+		team.stdDevWinDiff = stats.stdev(team.winDifferential[0:numWeeks])
+	return teams
+
+def plotWinDifferential(teams) :
+	weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+	plt.figure()
+	for team in teams :
+		plt.plot(weeks, team.winDifferential, '-^', label = team.name)
+	plt.ylabel("Win Differential")
+	plt.xlabel("Week")
+	plt.title("Win Differential Week By Week")
+	plt.legend(loc='upper left', bbox_to_anchor=(1, 1))	
+	plt.show()
+
 def setRank(teams) :
 	for week in range(0, numWeeks) :
 		prevMaxTeam = ''
@@ -98,8 +131,11 @@ def sortByRank(teams, week) :
 
 teams = getTeams(league)
 teams = setRecords(teams)
+teams = setWinDifferential(teams)
 teams = setRank(teams)
 teams = sortByRank(teams, numWeeks)
+teams = setStats(teams)
+#plotWinDifferential(teams)
 
 print('NDL POWER RANKINGS THROUGH WEEK ' + str(numWeeks))
 for t in teams :
@@ -107,25 +143,11 @@ for t in teams :
 	record = t.record[numWeeks-1]
 	rank = str(t.rank[numWeeks-1])
 	toPrint = rank + '. ' + name + ' ->'
-	toPrint += ' ' + str(record[WINS]) + '-' + str(record[LOSSES]) + '-' + str(record[TIES])
+	toPrint += ' ' + str(record[WINS]) + '-' + str(record[LOSSES]) + '-' + str(record[TIES]) + '.'
+	toPrint += ' WDA: ' + str(t.averageWinDiff)[0:4] + ', WDSD: ' + str(t.stdDevWinDiff)[0:4] + '.' 
+	toPrint += ' CM: ' + str(t.averageWinDiff / t.stdDevWinDiff)[0:4] + '.'
 	print(toPrint)
 
-
-# # sort the teams
-# teams = list(spr.keys());
-# rankedTeams = []
-# while len(teams) > 0 :
-# 	maxTeam = getMax(spr, teams)
-# 	rankedTeams.append(maxTeam)
-# 	teams.remove(maxTeam)
-
-# # print out the data
-# print('NDL POWER RANKINGS THROUGH WEEK ' + str(numWeeks))
-# for i in range(0, len(rankedTeams)) :
-# 	teamRank = spr[rankedTeams[i]]
-# 	toPrint = str(i+1) + '. ' + rankedTeams[i] + ' ->'
-# 	toPrint += ' ' + str(teamRank[0]) + '-' + str(teamRank[1]) + '-' + str(teamRank[2])
-# 	print(toPrint)
 
 
 
